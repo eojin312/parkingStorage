aa911417942829837bedfba38d02bfd90743ff62
eojin312 <eojin312@naver.com>
2022-07-05 21:59:59 +0900

    @RepeatedTest 과 RepetitionInfo 를 사용해서 테스트 반복하기

--- src/test/java/marsparkdream/parkingstorage/service/WeatherServiceImplTest.java
+++ src/test/java/marsparkdream/parkingstorage/service/WeatherServiceImplTest.java
@@ -3,6 +3,10 @@ package marsparkdream.parkingstorage.service;
 import lombok.extern.slf4j.Slf4j;
 import marsparkdream.parkingstorage.annotation.Description;
 import marsparkdream.parkingstorage.service.serviceInterface.WeatherService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,5 +24,18 @@ class WeatherServiceImplTest {
     @Test
     void getWeather() {
         log.info("weather info : {}", weatherService.getWeather());
+    }
+
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    @Test
+    @DisplayName("반복해서_테스트하기")
+    public void 반복해서_테스트하기 (RepetitionInfo repetitionInfo) {
+        // given
+        weatherService.getWeather();
+
+        // when
+        // 몇 번 반복했는지 표시
+        log.info("method for count : {} / total : {}", repetitionInfo.getCurrentRepetition(), repetitionInfo.getTotalRepetitions());
+        // then
     }
 }
